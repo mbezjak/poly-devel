d1f8092f63671be28eea88cf9ec2a810e74b35c2
Miro Bezjak <bezjak.miro@gmail.com>
2015-02-13 10:00:52 +0100

    example-projects: interact with a maven project

--- example-projects/showcase.sh
+++ example-projects/showcase.sh
@@ -3,9 +3,16 @@
 
 set -o errexit
 
+declare -r examples=$(dirname $(readlink -f "$0"))
+function switch-to {
+    local -r project="$1"
+    cd "$examples/$project"
+    msg "Switched to: $project"
+}
+function separate-project-output { newline; newline; }
 function interact { echo "[$(basename $PWD)] \$ $@"; "$@"; }
 
-cd gradle-library
+switch-to gradle-library
 interact status-for is-gradle
 interact project-dir
 interact project-name
@@ -21,4 +28,14 @@ interact update-dependency groovy-all 2.9.9
 interact grep groovy-all build.gradle
 interact update-dependency groovy-all 2.4.1
 
+
+separate-project-output
+switch-to maven-project
+interact status-for is-maven
+interact project-dir
+interact project-name
+interact project-group
+interact project-artifact
+
+newline
 exit 0
